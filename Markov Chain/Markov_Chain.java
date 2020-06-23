60a41535b86c4aed7e09e8a383542f7475aff651
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-06-23 21:53:25 +0530

    Update Markov_Chain.java

--- Markov Chain/Markov_Chain.java
+++ Markov Chain/Markov_Chain.java
@@ -1,1 +1,44 @@
+import java.io.*;
+import java.util.Scanner;
+import static java.lang.System.*;
 
+class rd
+{
+static int n;
+	static boolean markovChain()
+	{
+		double m[][]=new double[n][n];
+		double sum=0;
+		int s;
+		Scanner in = new Scanner(System.in);
+		// Checking validity of markov chain
+		for(int i = 0; i <n; i++)
+		{
+			for(int j = 0; j<n; j++)
+			{
+				out.println("Enter the value");
+				m[i][j]=in.nextFloat();
+				sum=sum+m[i][j];	
+			}	
+		}
+		s=(int)sum;
+		if(s==3)
+			return true;
+		else
+			return false;  
+	}
+	
+	public static void main(String[] args)
+	{
+		out.print("Hello");
+		Scanner in = new Scanner(System.in);
+		out.println("Enter size of Markov chain");
+		n=in.nextInt();
+		
+		if(markovChain())
+			out.println("Yes Markov chain");
+		else
+			out.println("No not markov chain");
+		out.println("End");
+	}	
+}
